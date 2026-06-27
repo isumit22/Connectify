061d1c3582c9c430c18014b445e195427f921839
Sumit Chaubey <139735352+isumit22@users.noreply.github.com>
2026-06-27 23:55:24 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set the base URL for all axios requests
 axios.defaults.baseURL =
-  process.env.REACT_APP_API_ORIGIN || "http://localhost:8000/api";
+(process.env.REACT_APP_API_ORIGIN || "http://localhost:8000") + "/api";
 
 // Set credentials to true
 axios.defaults.withCredentials = true;
